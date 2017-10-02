1531ebfed05c5ec14021595a81a538995f3fc429
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-02 13:21:09 -0400

    Add images, add header and footer component

---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer-container">
+        <a href="https://www.linkedin.com/in/jonathan-canfield-978641104/">
+          <img className="socialmediabutton" src="linkedinimg.png"/>
+        </a>
+        <a href="https://twitter.com/jtcanfield100">
+          <img className="socialmediabutton"  src="twitterlogo.png"/>
+        </a>
+        <a href="https://github.com/jtcanfield">
+          <img className="socialmediabutton" src="github.png"/>
+        </a>
+      </div>
+    );
+  }
+}
---
+++ src/components/topbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+export default class TopBar extends Component {
+  render() {
+    return (
+      <div className="topbar-container">
+        <h1 className="name-title">Jonathan Canfield</h1>
+      </div>
+    );
+  }
+}
